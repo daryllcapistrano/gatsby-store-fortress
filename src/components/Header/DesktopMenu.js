06facb5fc0c1a8cde14671e335b1e0e112f284bd
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-04-10 20:19:14 -0700

    fix errors

--- src/components/Header/DesktopMenu.js
+++ src/components/Header/DesktopMenu.js
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from 'react'
 import {Link, withPrefix} from 'gatsby'
-import {Menu, Container, Icon} from 'semantic-ui-react'
+import {Menu, Container} from 'semantic-ui-react'
 import ShoppingCartIcon from './ShoppingCartIcon'
 import Logo from './Logo'
 
-const DesktopMenu = ({location: {pathname}, token, cartCount, signout}) => {
+const DesktopMenu = ({location: {pathname}, token, cartCount}) => {
   const [activeItem, setActiveItem] = useState(pathname)
 
   useEffect(() => {
@@ -14,26 +14,17 @@ const DesktopMenu = ({location: {pathname}, token, cartCount, signout}) => {
   return (
     <Menu size="huge" borderless pointing>
       <Container text>
-        <Menu.Item
-          active={activeItem === withPrefix('/')}
-          as={Link}
-          to="/"
-          header
-        >
+        <Menu.Item as={Link} to="/" header>
           <Logo />
-          Starter Store
+          Fortress Skate Co.
         </Menu.Item>
         {token ? (
           <Menu.Menu position="right">
-            <Menu.Item
-              as={Link}
-              to="/myaccount/"
-              active={activeItem === withPrefix('/myaccount/')}
-            >
-              <Icon name="user" />
-              My Account
-            </Menu.Item>
-            <Menu.Item onClick={signout}>Sign out</Menu.Item>
+            {/* <Menu.Item as={Link} to="/myaccount/" active={activeItem === withPrefix('/myaccount/')}>
+							<Icon name="user" />
+							My Account
+						</Menu.Item>
+						<Menu.Item onClick={signout}>Sign out</Menu.Item> */}
             <Menu.Item
               as={Link}
               to="/cart/"
@@ -44,20 +35,12 @@ const DesktopMenu = ({location: {pathname}, token, cartCount, signout}) => {
           </Menu.Menu>
         ) : (
           <Menu.Menu position="right">
-            <Menu.Item
-              as={Link}
-              to="/register/"
-              active={activeItem === withPrefix('/register/')}
-            >
-              Sign up
-            </Menu.Item>
-            <Menu.Item
-              as={Link}
-              to="/login/"
-              active={activeItem === withPrefix('/login/')}
-            >
-              Sign in
-            </Menu.Item>
+            {/* <Menu.Item as={Link} to="/register/" active={activeItem === withPrefix('/register/')}>
+							Sign up
+						</Menu.Item>
+						<Menu.Item as={Link} to="/login/" active={activeItem === withPrefix('/login/')}>
+							Sign in
+						</Menu.Item> */}
             <Menu.Item
               as={Link}
               to="/cart/"
